b989718048b3a02d31041fc60f68b754a7fc9059
Ben Wetherfield <bwetherfield@gmail.com>
2019-06-16 16:39:00 -0700

    Prove PitchedEdge Encodability

--- Sources/ContextFreeSpellingInverter/main.swift
+++ Sources/ContextFreeSpellingInverter/main.swift
@@ -1,1 +1,21 @@
-print("Hello, world!")
+import Foundation
+import DataStructures
+import Pitch
+import SpelledPitch
+import PitchSpeller
+
+let encoder = JSONEncoder()
+encoder.outputFormatting = .prettyPrinted
+let data = try encoder.encode(spellings)
+
+let pair = OrderedPair<Int> (3, 4)
+
+let pairData = try encoder.encode(pair)
+
+let pitchA: Pitch.Spelling = .c
+let pitchB: Pitch.Spelling = .d
+
+let pitchPair: OrderedPair<Cross<Pitch.Class, Tendency>> = .init(.init(3, .up), .init(4, .down))
+
+
+let pitchPairData = try encoder.encode(pitchPair)
