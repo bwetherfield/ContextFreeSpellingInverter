e9520665da4cdf2cb31e422fcd0a8f487dd3337b
Ben Wetherfield <bwetherfield@gmail.com>
2019-06-16 11:04:59 -0700

    Add GlobalSpellings file

---
+++ Sources/ContextFreeSpellingInverter/GlobalSpellings.swift
@@ -0,0 +1,14 @@
+//
+//  GlobalSpellings.swift
+//  ContextFreeSpellingInverter
+//
+//  Created by Benjamin Wetherfield on 6/16/19.
+//
+
+import Pitch
+import SpelledPitch
+import PitchSpeller
+
+let spellings: [[Pitch.Spelling]] = [
+    []
+]
