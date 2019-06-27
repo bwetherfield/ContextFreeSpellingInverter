e3bbce7d628d66009660efc1c876937b4fc1f0ed
Ben Wetherfield <bwetherfield@gmail.com>
2019-06-27 15:12:14 -0700

    Fix default test

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/bwetherfield/PitchSpeller", .branch("pitchspeller-dependency"))
+    .package(url: "https://github.com/bwetherfield/PitchSpeller", .branch("pitchspeller-dependency")),
+    .package(url: "https://github.com/dn-m/Structure", .branch("pitchspeller-dependency"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
