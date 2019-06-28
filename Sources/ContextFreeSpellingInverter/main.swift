4fc5e910eefe15821aa6a2b65b08bedf85682613
Ben Wetherfield <bwetherfield@gmail.com>
2019-06-27 17:14:56 -0700

    Add file saving to json

--- Sources/ContextFreeSpellingInverter/main.swift
+++ Sources/ContextFreeSpellingInverter/main.swift
@@ -1,6 +1,11 @@
 import Foundation
 import PitchSpeller
 
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
+
 let spellingInverter = SpellingInverter(spellings: spellings)
 let lookupTable = spellingInverter.generateWeights()
 
@@ -8,4 +13,8 @@ let encoder = JSONEncoder()
 encoder.outputFormatting = .prettyPrinted
 let data = try encoder.encode(lookupTable)
 
-print(String(data: data, encoding: .utf8)!)
+let string = String(data: data, encoding: .utf8)!
+
+let filename = getDocumentsDirectory().appendingPathComponent("pitched_weights.json")
+
+try! string.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
